d63f0a00ca2e4bf723734b01488eda93e99b3d90
fsanchezfns <fsanchez.fns@gmail.com>
2019-01-12 18:57:36 -0300

    base de datos actualizada

--- integradorfranco.sql
+++ integradorfranco.sql
@@ -4,9 +4,9 @@
  * Created: 17/10/2018
  */
 
-CREATE DATABASE `integradorfranco`
+CREATE DATABASE `integradorfranco`;
 
-USE `integradorfranco`
+USE `integradorfranco`;
 
 DROP TABLE IF EXISTS  `perro`;
 
@@ -18,17 +18,4 @@ CREATE TABLE `perro`(
 PRIMARY KEY (`id_perro`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
-insert into `perro` (`id_perro`,`nombre_perro`,`raza_perro`,`edad_perro`) values ('','luna','coli',34)
-
-	DROP TABLE IF EXISTS  `perro2`;
-
-CREATE TABLE `perro2`(
-`id_perro` int(11) NOT NULL AUTO_INCREMENT,
-`nombre_perro` varchar(40) DEFAULT NULL,
-`raza_perro` varchar(40) DEFAULT NULL,
-`edad_perro` int(3) DEFAULT NULL,
-`otro` int(3) DEFAULT NULL,
-PRIMARY KEY (`id_perro`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
-
-insert into `perro` (`id_perro`,`nombre_perro`,`raza_perro`,`edad_perro`) values ('','luna','coli',34)
+insert into `perro` (`id_perro`,`nombre_perro`,`raza_perro`,`edad_perro`) values ('','luna','coli',34);
